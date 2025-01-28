b889551281a457777cfd9f722e84ecf62bff30a0
Vinicius Soares <suiciniv-s@hotmail.com>
2025-01-28 00:48:04 -0300

    For

--- primeira_app/app1.py
+++ primeira_app/app1.py
@@ -1,5 +1,7 @@
 import os
 
+restaurantes = ['Pizza','Lasanha','Pastel','Sushi']
+
 def nome_programa():
 
     print("""
@@ -28,10 +30,29 @@ def finalizar_app():
     print('Finalizando App\n')
     exit()
 
-
 def opcao_invalida():
     print('Opção Inválida\n')
     input('Pressione ENTER para continuar...')
+    main()
+
+def cadastrar_restaurante():
+    os.system('clear')
+    print('Cadastro de novos restaurantes\n')
+    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar:')
+    restaurantes.append(nome_do_restaurante)
+    print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+    input('Digite uma tecla para voltar ao menu principal.')
+    main()
+
+
+def listar_restaurantes():
+    os.system('clear')
+    print('Lista de restaurantes cadastrados\n')
+
+    for restaurante in restaurantes:
+        print(f'- {restaurante}')
+
+    input('\nDigite uma tecla para voltar ao menu principal.')
     main()
 
 
@@ -43,18 +64,18 @@ def opcoes():
 
         match opcao_escolhida:
             case 1:
-                print('Cadastrar Restaurante')
+                cadastrar_restaurante()
             case 2:
-                print('Listar Restaurantes')
+                listar_restaurantes()
             case 3:
                 print('Ativar Restaurante')
             case 4:
                 finalizar_app()
+                return
             case _:
                 opcao_invalida()
-    except:
-        opcao_invalida()
-
+    except ValueError:
+        print("Opção inválida, por favor insira um número!\n")
 
 def main():
     os.system('clear')
